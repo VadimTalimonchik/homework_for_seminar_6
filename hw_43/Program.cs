40361ae27fb10717368ffb181f497bd936d85b08
VadimTalimonchik <tvadmins@gmail.com>
2023-01-06 09:50:37 +0300

    Задача 43

--- hw_43/Program.cs
+++ hw_43/Program.cs
@@ -7,22 +7,22 @@
 void DotIntersection()
 {
 
-Console.Write("Введите k1: ");
-var k1 = Convert.ToDouble(Console.ReadLine()!);
 Console.Write("Введите b1: ");
 var b1 = Convert.ToDouble(Console.ReadLine()!);
-Console.Write("Введите k2: ");
-var k2 = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine()!);
 Console.Write("Введите b2: ");
 var b2 = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine()!);
 
-var x = -(b1 - b2) / (k1 - k2);
+var x = (b2 - b1) / (k1 - k2);
 var y = k1 * x + b1;
 
 x = Math.Round(x, 3);
 y = Math.Round(y, 3);
 
-Console.WriteLine($"Координаты точки пересечения x и y двух прямых: ({x}; {y})");
+Console.WriteLine($"Координаты точки пересечения x и y двух прямых равны: ({x}; {y})");
 
 }
 DotIntersection();
